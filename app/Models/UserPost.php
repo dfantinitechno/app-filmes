cab6d7b4d9374325b5c932123fb4ad0740823ff4
Diego Fantini <dfantinitechno@hotmail.com>
2023-03-26 23:59:48 -0300

    app filmes model

--- app/Models/UserPost.php
+++ app/Models/UserPost.php
@@ -33,6 +33,7 @@ class UserPost extends Model
             ->leftJoin(DB::raw('(SELECT user_post_liked.id_post, COUNT(user_post_liked.id_post) AS count_liked FROM user_post AS user_post_liked WHERE user_post_liked.type = 1 GROUP BY user_post_liked.id_post) as liked'), 'liked.id_post', '=', 'posts.id')
             ->leftJoin(DB::raw('(SELECT user_post_followed.id_post, COUNT(user_post_followed.id_post) AS count_followed FROM user_post AS user_post_followed WHERE user_post_followed.type = 2 GROUP BY user_post_followed.id_post) as followed'), 'followed.id_post', '=', 'posts.id')
             ->leftJoin(DB::raw('(SELECT user_post_unliked.id_post, COUNT(user_post_unliked.id_post) AS count_unliked FROM user_post AS user_post_unliked WHERE user_post_unliked.type = 3 GROUP BY user_post_unliked.id_post) as unliked'), 'unliked.id_post', '=', 'posts.id')
+            ->where('user_post.id_user', $id_user)
             ->groupBy('posts.id')
             ->orderBy('user_post.updated_at', 'desc')
             ->get();
